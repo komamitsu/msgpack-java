fb727748586faf1f6a55abb9faacc0ca0dadfc9d
Mitsunori Komatsu <komamitsu@gmail.com>
2025-01-01 19:59:46 +0900

    Tidy

--- msgpack-jackson/src/main/java/org/msgpack/jackson/dataformat/MessagePackGenerator.java
+++ msgpack-jackson/src/main/java/org/msgpack/jackson/dataformat/MessagePackGenerator.java
@@ -30,7 +30,6 @@ import org.msgpack.core.buffer.OutputStreamBufferOutput;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
