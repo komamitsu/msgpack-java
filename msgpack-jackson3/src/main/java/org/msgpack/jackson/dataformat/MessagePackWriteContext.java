84c0955b0d56db6dcc09fa367a57cb3c42fb5c10
Mitsunori Komatsu <komamitsu@gmail.com>
2026-05-27 14:26:26 +0900

    Move MessagePackWriteContext to top-level class and add unit tests
    Extracted the private inner class from MessagePackGenerator into its own
    package-level file, consistent with MessagePackReadContext. Added
    MessagePackWriteContextTest covering root/array/object context state,
    index and entry count tracking, parent references, and currentValue.

---
+++ msgpack-jackson3/src/main/java/org/msgpack/jackson/dataformat/MessagePackWriteContext.java
@@ -0,0 +1,98 @@
+//
+// MessagePack for Java
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+package org.msgpack.jackson.dataformat;
+
+import tools.jackson.core.TokenStreamContext;
+
+class MessagePackWriteContext extends TokenStreamContext
+{
+    private final MessagePackWriteContext parent;
+    private String currentName;
+    private Object currentValue;
+    // For TYPE_OBJECT: true after writeName (expecting value), false after writeValue (expecting name)
+    private boolean gotName;
+
+    private MessagePackWriteContext(int type, MessagePackWriteContext parent)
+    {
+        super(type, -1);
+        this.parent = parent;
+    }
+
+    static MessagePackWriteContext createRootContext()
+    {
+        return new MessagePackWriteContext(TYPE_ROOT, null);
+    }
+
+    MessagePackWriteContext createChildArrayContext(Object value)
+    {
+        MessagePackWriteContext ctx = new MessagePackWriteContext(TYPE_ARRAY, this);
+        ctx.currentValue = value;
+        return ctx;
+    }
+
+    MessagePackWriteContext createChildObjectContext(Object value)
+    {
+        MessagePackWriteContext ctx = new MessagePackWriteContext(TYPE_OBJECT, this);
+        ctx.currentValue = value;
+        return ctx;
+    }
+
+    @Override
+    public MessagePackWriteContext getParent()
+    {
+        return parent;
+    }
+
+    boolean writeValue()
+    {
+        if (_type == TYPE_OBJECT) {
+            if (!gotName) {
+                return false;
+            }
+            gotName = false;
+        }
+        ++_index;
+        return true;
+    }
+
+    boolean writeName(String name)
+    {
+        if (_type != TYPE_OBJECT || gotName) {
+            return false;
+        }
+        currentName = name;
+        gotName = true;
+        return true;
+    }
+
+    @Override
+    public String currentName()
+    {
+        return currentName;
+    }
+
+    @Override
+    public Object currentValue()
+    {
+        return currentValue;
+    }
+
+    @Override
+    public void assignCurrentValue(Object v)
+    {
+        currentValue = v;
+    }
+}
